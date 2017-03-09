7485795f4c3f4310cd2652b4484f665ea9335f9d
lishuo <lishuo98@yahoo.com.sg>
2017-03-09 12:52:05 +0800

    Changed images

--- main/predict.py
+++ main/predict.py
@@ -1,19 +1,19 @@
 from model import load_model
 
 def predict():
-    file = input('Name of image to be classfied(file[0 - 13]), or own file:\n')
+    file = input('Name of image to be classfied(file[0 - 12]), or own file:\n')
     try:
         a = int(file)
         file = 'file' + str(a)
-        line = int(load_model(file))
+        answer = int(load_model(file))
     except:
-        line = int(load_model(file))
+        answer = int(load_model(file))
 
     categories = []
     with open('../data/words.txt', 'r') as r:
         for line in r:
             a = line.split()
             categories.append(a[1 : ])
-    return categories[line]
+    return categories[answer]
 if __name__ == '__main__':
     print(predict())
